5c8788357f0c84ebf5358d3f3ac401475de64067
afloresep <afloresep01@gmail.com>
2026-01-19 15:28:18 +0100

    add cluster_io utilities to  __init__.py

--- chelombus/utils/__init__.py
+++ chelombus/utils/__init__.py
@@ -1,2 +1,18 @@
-from .fingerprints import FingerprintCalculator 
+from .fingerprints import FingerprintCalculator
 from .helper_functions import format_time, get_time, save_chunk
+
+# Cluster I/O utilities (require duckdb)
+try:
+    from .cluster_io import (
+        query_cluster,
+        export_cluster,
+        export_all_clusters,
+        get_cluster_stats,
+        get_cluster_ids,
+        get_total_molecules,
+        query_clusters_batch,
+        sample_from_cluster,
+    )
+except ImportError:
+    # duckdb not installed - cluster I/O functions not available
+    pass
